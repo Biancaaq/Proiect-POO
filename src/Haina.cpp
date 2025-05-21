a4ab74963ba400c8520ead12d0a620c0bd1715b1
Biancaaq <draghici.bianca1975@gmail.com>
2025-05-21 10:55:44 +0300

    Am scos implementarea functiei upgrade din clasa Haina, deoarece este virtual pura

--- src/Haina.cpp
+++ src/Haina.cpp
@@ -3,5 +3,3 @@
 
 Haina::Haina(const std::string &nume, double greutate, int nivel, int bonusStamina, int bonusViteza, int bonusScadereStamina, double bonusBuzunar) : Obiect(nume, greutate), nivel(nivel), bonusStamina(bonusStamina), bonusViteza(bonusViteza), bonusScadereStamina(bonusScadereStamina), bonusBuzunar(bonusBuzunar) {}
 Haina::~Haina() = default;
-
-bool Haina::upgrade(int& bani, Rucsac& rucsac) {}
