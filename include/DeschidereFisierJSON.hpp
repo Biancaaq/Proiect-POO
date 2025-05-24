5c458951588be5f61ecba059e4f9c3994a61c512
Biancaaq <draghici.bianca1975@gmail.com>
2025-05-24 03:57:40 +0300

    Incercare rezolvare eroare fisiere JSON

--- include/DeschidereFisierJSON.hpp
+++ include/DeschidereFisierJSON.hpp
@@ -4,14 +4,29 @@
 #include <fstream>
 #include <stdexcept>
 #include <string>
+#include <filesystem>
+#include <iostream>
 
 inline std::ifstream deschideFisierJson(const std::string& nume) {
-    std::ifstream f(nume);
-    if (!f.is_open())
-        f.open("../" + nume);
-    if (!f.is_open())
-        throw std::runtime_error("Nu s-a putut deschide fișierul " + nume);
-    return f;
+    std::ifstream f;
+
+    std::string variante[] = {
+        nume,
+        "resurse/" + nume,     
+        "../resurse/" + nume
+    };
+
+    for (const auto& cale : variante) {
+        f.open(cale);
+        if (f.is_open()) {
+            std::cout << "[OK] Deschis fisier: " << cale << "\n";
+            return f;
+        }
+    }
+
+    std::cerr << "[Eroare] Nu s-a putut deschide fișierul " << nume << "\n";
+    std::cerr << "[DEBUG] current_path(): " << std::filesystem::current_path() << "\n";
+    throw std::runtime_error("Nu s-a putut deschide fișierul " + nume);
 }
 
 #endif
